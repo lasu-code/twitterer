9d6baabdee10bdaa78f3b5de72464c57cce18fbe
Jubril Issa <issa.jubril@andela.com>
2018-09-19 07:37:24 +0100

    Initial commit while teaching

---
+++ controllers/tweetController.js
@@ -0,0 +1,32 @@
+let userTweets = [
+    {
+        body: "My first tweet",
+        user: "Al Amin",
+        date: new Date
+    },
+    {
+        body: "My second tweet",
+        user: "Al Lagabja",
+        date: new Date
+    },
+    {
+        body: "My third tweet",
+        user: "Al Jagbajantis",
+        date: new Date
+    },
+]
+
+exports.homePage = function(req, res, next) {
+  res.render('index', { title: 'LASU' });
+};
+
+exports.tweets = function(req, res, next) {
+
+  res.render('tweet', { title: 'My tweets', tweets: userTweets });
+};
+
+exports.profilePage = function(req, res, next) {
+    
+    res.render('profile', { title: 'My tweets', username: req.params.name });
+  }
+
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const tweetController = require('../controllers/tweetController');
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'LASU' });
+// });
+
+router.get('/', tweetController.homePage);
+router.get('/tweet', tweetController.tweets);
+router.get('/:name', tweetController.profilePage);
+
+// router.get('/tweet', function(req, res, next) {
+//   res.render('tweet', { title: 'My tweets', tweets: "Uwawu pawpaw mehn Yaw!!!" });
+// });
+
+// router.get('/:name', function(req, res, next) {
+//   res.render('profile', { title: 'My tweets', username: req.params.name });
+// });
+
+module.exports = router;
