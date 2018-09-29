6638f84c01a581ffcb3852bfab7ff757d718925d
Jubril Issa <issa.jubril@andela.com>
2018-09-29 12:27:00 +0100

    Added read functionality from database

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -6,22 +6,27 @@ let userTweets = [
   {
     body: 'My first tweet',
     user: 'Al Amin',
-    date: new Date(),
+    createdDate: new Date(),
   },
   {
     body: 'My second tweet',
     user: 'Al Lagabja',
-    date: new Date(),
+    createdDate: new Date(),
   },
   {
     body: 'My third tweet',
     user: 'Al Jagbajantis',
-    date: new Date(),
+    createdDate: new Date(),
   },
 ];
 
 exports.homePage = function(req, res, next) {
-  res.render('index', { title: 'LASUCode' });
+  Tweet.find().
+    then(function(tweets) {
+      console.log(tweets)
+      res.render('tweet', { title: 'My tweets', tweets: tweets });
+      // res.render('index', { title: 'LASUCode' });
+    })
 };
 
 // exports.tweets = function(req, res, next) {
